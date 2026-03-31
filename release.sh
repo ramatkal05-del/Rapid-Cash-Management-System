022f066edfaad79abfe2d24218c48c9511ab0121
ramatkal05 <ramatkal05@gmail.com>
2026-03-31 23:24:47 +0300

    Update admin credentials to kizy/789456+++

--- release.sh
+++ release.sh
@@ -15,11 +15,28 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.production')
 django.setup()
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@rapid-cash.com', '123456+++')
-    print('Superuser admin created!')
+if not User.objects.filter(username='kizy').exists():
+    user = User.objects.create_superuser('kizy', 'kizy@rapid-cash.com', '789456+++')
+    user.first_name = 'Admin'
+    user.last_name = 'Kizy'
+    user.save()
+    print('Superuser kizy created!')
 else:
-    print('Superuser admin already exists.')
+    print('Superuser kizy already exists.')
+"
+
+echo "Unlocking kizy account if locked..."
+python -c "
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.production')
+django.setup()
+try:
+    from axes.utils import reset
+    reset(username='kizy')
+    print('Kizy account unlocked!')
+except Exception as e:
+    print(f'Note: Could not unlock kizy: {e}')
 "
 
 echo "Release commands completed!"

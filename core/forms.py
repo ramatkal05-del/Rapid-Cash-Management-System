7ead7366d3740f82348c1659ed9f8696ec617a55
ramatkal05 <ramatkal05@gmail.com>
2026-04-04 18:31:08 +0300

    Fix all currency/caisse imports - correct module paths

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from finance.models import Currency, PartnerContract
+from core.models import Currency
+from finance.models import PartnerContract
 
 User = get_user_model()
 
